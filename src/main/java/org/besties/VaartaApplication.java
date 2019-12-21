1857457b87d5e4ca674d6fe93138cdfeb5fe1ffc
Dhrubajyoti Gogoi <d3vel0per@users.noreply.github.com>
2019-12-21 22:45:51 +0530

    First draft. Frontend only

---
+++ src/main/java/org/besties/VaartaApplication.java
@@ -0,0 +1,11 @@
+package org.besties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaartaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VaartaApplication.class, args);
+    }
+}
